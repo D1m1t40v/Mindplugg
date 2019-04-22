05dee707b635c4211113c1bc3823a418ccf00fa1
D1m1t40v <dimitri.quesnel@gmail.com>
2019-04-22 23:25:18 +0200

    Implementation with tab

--- Mindplugg/CircularBuffer.cs
+++ Mindplugg/CircularBuffer.cs
@@ -1,38 +1,110 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 
 namespace Mindplugg
 {
     public class CircularBuffer<T> : IEnumerable<T>
     {
+        private int headIndex = -1;
+        private int tailIndex = 0;
+        private T[] tab;
+        private bool hasBeenFull = false;
+
+        public readonly int Size;
+
         public CircularBuffer(int capacity)
         {
-            throw new NotImplementedException();
+            Size = capacity;
+            tab = new T[Size];
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            headIndex++;
+            if (hasBeenFull)
+            {
+                tailIndex++;
+            }
+
+            if (headIndex >= tab.Length)
+            {
+                headIndex = 0;
+                tailIndex = 1;
+                hasBeenFull = true;
+            }
+
+            tab[headIndex] = item;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            tab = new T[Size];
+            headIndex = -1;
+            tailIndex = 0;
         }
 
         #region IEnumerable
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new BufferEnumerator(this);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new BufferEnumerator(this);
         }
 
         #endregion
+
+        #region IEnumerator
+        private class BufferEnumerator : IEnumerator<T>
+        {
+            private CircularBuffer<T> _parentBuffer;
+            private int readIndex;
+            private bool ignoreFirstHeadIndex;
+
+            public BufferEnumerator(CircularBuffer<T> parentBuffer)
+            {
+                _parentBuffer = parentBuffer;
+                readIndex = _parentBuffer.tailIndex - 1;
+                ignoreFirstHeadIndex = _parentBuffer.hasBeenFull;
+            }
+
+            public T Current => _parentBuffer.tab[readIndex];
+
+            object IEnumerator.Current => _parentBuffer.tab[readIndex];
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (readIndex == _parentBuffer.headIndex)
+                {
+                    if (ignoreFirstHeadIndex)
+                    {
+                        ignoreFirstHeadIndex = false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (readIndex >= _parentBuffer.Size - 1)
+                {
+                    readIndex = 0;
+                    return true;
+                }
+
+                readIndex++;
+                return true;
+            }
+
+            public void Reset()
+            {
+                readIndex = _parentBuffer.tailIndex - 1;
+            }
+        }
+        #endregion IEnumerator
     }
 }
